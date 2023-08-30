3d08cb3326f3492394cc71f55176ad6816156da4
Seohabin6078 <habin6078@gmail.com>
2023-08-30 15:41:48 +0900

    first commit

---
+++ src/main/java/com/yamukja/YamukjaApplication.java
@@ -0,0 +1,13 @@
+package com.yamukja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamukjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamukjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yamukja/YamukjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamukja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamukjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
